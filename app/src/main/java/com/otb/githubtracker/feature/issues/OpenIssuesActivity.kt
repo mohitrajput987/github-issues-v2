6daff8a8cd555bca06fce7f5eb79ab58213b7d19
Mohit Rajput <mohitrajput987@gmail.com>
2022-11-26 21:53:26 +0530

    refactor: deleted xml implementation of open issues

--- app/src/main/java/com/otb/githubtracker/feature/issues/OpenIssuesActivity.kt
+++ app/src/main/java/com/otb/githubtracker/feature/issues/OpenIssuesActivity.kt
@@ -1,72 +1,36 @@
 package com.otb.githubtracker.feature.issues
 
-import android.view.LayoutInflater
-import android.widget.Toast
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.otb.githubtracker.common.LoadingSpinner
-import com.otb.githubtracker.common.base.BaseActivity
 import com.otb.githubtracker.common.base.DisplaysLoadingSpinner
-import com.otb.githubtracker.databinding.ActivityOpenIssuesBinding
 import com.otb.githubtracker.feature.comments.CommentsActivity
-import com.otb.githubtracker.network.ViewState
+import com.otb.githubtracker.feature.issues.ui.OpenIssuesScreen
+import com.otb.githubtracker.theme.GitHubIssuesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class OpenIssuesActivity : BaseActivity<ActivityOpenIssuesBinding>(), DisplaysLoadingSpinner {
+class OpenIssuesActivity : ComponentActivity(), DisplaysLoadingSpinner {
     companion object {
-        private const val DEFAULT_ORG_NAME = "square"
-        private const val DEFAULT_REPO_NAME = "okhttp"
+        const val DEFAULT_ORG_NAME = "square"
+        const val DEFAULT_REPO_NAME = "okhttp"
     }
-
-    override fun inflateLayout(layoutInflater: LayoutInflater) =
-        ActivityOpenIssuesBinding.inflate(layoutInflater)
 
     private val viewModel by viewModels<OpenIssuesViewModel>()
-    private val openIssuesAdapter by lazy {
-        OpenIssuesAdapter(onItemClick = { issueEntity ->
-            onIssueItemClick(issueEntity)
-        })
-    }
     override val loadingSpinner by lazy { LoadingSpinner(this) }
 
-    override fun setupView() {
-        setupRecyclerView()
-        observeIssues()
-        viewModel.fetchIssues(DEFAULT_ORG_NAME, DEFAULT_REPO_NAME)
-    }
-
-    private fun setupRecyclerView() {
-        val verticalLayoutManager = LinearLayoutManager(this@OpenIssuesActivity)
-        binding.rvIssues.apply {
-            layoutManager = verticalLayoutManager
-            adapter = openIssuesAdapter
-            addItemDecoration(DividerItemDecoration(baseContext, verticalLayoutManager.orientation))
-        }
-    }
-
-    private fun observeIssues() {
-        viewModel.openIssuesLiveData.observe(this) {
-            when (it) {
-                is ViewState.Loading -> showLoadingSpinner()
-                is ViewState.Success -> showIssues(it.data)
-                is ViewState.Error -> showErrorMessage(it.errorMessage)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GitHubIssuesTheme {
+                OpenIssuesScreen(activity = this, viewModel = viewModel)
             }
         }
     }
 
-    private fun showErrorMessage(errorMessage: String) {
-        dismissLoadingSpinner()
-        Toast.makeText(applicationContext, errorMessage, Toast.LENGTH_SHORT).show()
-    }
-
-    private fun showIssues(issueEntities: List<OpenIssuesModels.IssueEntity>) {
-        dismissLoadingSpinner()
-        openIssuesAdapter.submitList(issueEntities)
-    }
-
-    private fun onIssueItemClick(issueEntity: OpenIssuesModels.IssueEntity) {
+    fun onIssueItemClick(issueEntity: OpenIssuesModels.IssueEntity) {
         startActivity(CommentsActivity.getIntent(this, issueEntity))
     }
 }
